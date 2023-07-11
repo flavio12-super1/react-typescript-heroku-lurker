0e3ab08848afc1f3a1992f9a576d6b27bad33705
flavio herrera <herreraflavio0@gmail.com>
2023-07-11 14:21:59 -0700

    fixed overflow on landing page

--- frontend/src/components/landingPageComponents/About.js
+++ frontend/src/components/landingPageComponents/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
   return (
-    <div>
+    <div id="aboutLurkerContainer">
       <div id="aboutLurker">
         <div className="textOne">A place for Nerds</div>
         <div className="textTwo">
@@ -11,7 +11,7 @@ function About() {
         </div>
       </div>
 
-      <div>
+      <div id="reqOuterContainerDiv">
         <div className="reqOuterContainer">
           <div className="textOne">
             Want to help out? Things that need to be updated:
@@ -74,6 +74,47 @@ function About() {
                   <div className="indivText">
                     - Add error handling to the front end and backend (incorrect
                     password, rate limited) .
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="textTwo">Update email to username</div>
+                <div className="containerText">
+                  <div className="indivText">
+                    - Instead of using your email to sign up for a new account
+                    it should be updated to a unique username
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="textTwo">Implement email and phone backup</div>
+                <div className="containerText">
+                  <div className="indivText">
+                    - Implement a method to allow users to add an email or phone
+                    backup.
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="textTwo">Implement email verification</div>
+                <div className="containerText">
+                  <div className="indivText">
+                    - All new accounts should be verified with an email as to
+                    prevent people from creating spam accounts
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="textTwo">Implement forgot password</div>
+                <div className="containerText">
+                  <div className="indivText">
+                    - Implement a method to allow users to recover their
+                    password if they forgot it, this should use email
+                    verification .
                   </div>
                 </div>
               </div>
